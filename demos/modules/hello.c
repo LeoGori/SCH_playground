dc705b6f544ba110604e61680329b85e4cf7e174
Riccardo250 <riccardo.lamarca01@gmail.com>
2023-10-11 22:06:01 +0200

    sys_call_table leaked

--- demos/modules/hello.c
+++ demos/modules/hello.c
@@ -1,15 +1,41 @@
 #include <linux/init.h>   // included for __init and __exit macros
 #include <linux/kernel.h> // included for KERN_INFO
 #include <linux/module.h> // included for all kernel modules
+#include <linux/kprobes.h> 
 
 MODULE_LICENSE("GPL"); // The license  under which the module is distributed.
 MODULE_AUTHOR("Girish Joshi"); // The original author of the module (VZ).
 MODULE_DESCRIPTION(
     "HelloWorld Linux Kernel Module."); // The Description of the module.
 
+
+typedef void ** (*FunctionPtr)(char*);
+
+static char symbol[KSYM_NAME_LEN] = "kallsyms_lookup_name";
+
+static struct kprobe kp = {
+	.symbol_name	= symbol,
+};
+
+
+static void ** leak_sys_call_table(void) {
+  int ret = register_kprobe(&kp);
+  // if (ret < 0) {
+  //   pr_err("register_kprobe failed, returned %d\n", ret);
+  //   return ret;
+  // }
+  pr_info("kallsyms_lookup_name = %llx\n", kp.addr);
+
+  FunctionPtr to_call = (FunctionPtr)kp.addr;
+  void ** addr = to_call("sys_call_table");
+  return addr;
+};
+
 // This function defines what happens when this module is inserted into the
 // kernel. ie. when you run insmod command.
 static int __init hello_init(void) {
+  pr_info("sys_call_table address = %llx\n", leak_sys_call_table());
+
   printk(KERN_INFO "Hello world!\n");
   return 0; // Non-zero return means that the module couldn't be loaded.
 }
--- scripts/build-k.sh
+++ scripts/build-k.sh
@@ -10,8 +10,7 @@ sed -i "s/CONFIG_RANDOMIZE_BASE=y/CONFIG_RANDOMIZE_BASE=n/g" .config
 sed -i '$a\CONFIG_FRAME_POINTER=y' .config
 sed -i '$a\CONFIG_DEBUG_INFO_REDUCED=n' .config
 sed -i '$a\CONFIG_GDB_SCRIPTS=y' .config
-
-
+sed -i '$a\CONFIG_KALLSYMS_ALL=y' .config
 make -j4 bzImage
 
 
